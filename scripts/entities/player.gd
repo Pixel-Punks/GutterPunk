2063caa5f01e0d2e9bfec9fe8e56e05206d96807
Julmy Dimitri <dim.julmy@gmail.com>
2024-07-24 16:27:23 +0200

    [feat] add paused menue

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -4,14 +4,21 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+signal pause_menu()
+
 func _physics_process(_delta):
 	handle_attack()
 	handle_movement()
+	handle_menu()
 
 func handle_attack():
 	if Input.is_action_pressed("player_attack"):
 		if weapon && weapon.has_method(("attack")):
 			weapon.attack()
+
+func handle_menu():
+	if Input.is_action_pressed("open_menu"):
+		pause_menu.emit()
 
 func handle_movement():
 	var input_direction = Input.get_vector(
