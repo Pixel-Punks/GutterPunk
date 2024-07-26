b9d83ea36c66cf7bf0ff5a905b78cf6dcae2d8d1
Julmy Dimitri <dim.julmy@gmail.com>
2024-07-26 20:58:36 +0200

    [feat] randomize money drop

--- scenes/components/moneydrop_component.gd
+++ scenes/components/moneydrop_component.gd
@@ -3,16 +3,12 @@ extends Node2D
 @onready var drop = preload("res://scenes/entities/coin.tscn")
 
 func _on_tree_exiting():
-	print("here exiting")
-	var parent = get_parent()
-	print(parent)
-	var coin = drop.instantiate()
-	print(coin)
-	var pos = parent.global_position
-	print(pos)
-	parent = parent.get_parent()
-	print(parent)
-	#parent.add_child(coin)
-	get_tree().root.add_child(coin)
-	coin.global_position = global_position
-	coin.z_index = 10
+	var rng = RandomNumberGenerator.new()
+	var rnd = rng.randi_range(1,2)
+	if rnd == 2:
+		var parent = get_parent()
+		var coin = drop.instantiate()
+		var pos = parent.global_position
+		parent = parent.get_parent()
+		get_tree().root.add_child(coin)
+		coin.global_position = global_position
