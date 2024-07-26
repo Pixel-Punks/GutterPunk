307ce728bbfcb402cb07fd0fae05d51f6f375b9b
Julmy Dimitri <dim.julmy@gmail.com>
2024-07-26 10:31:09 +0200

    [feat] finish menue

--- scripts/menues/start_menu.gd
+++ scripts/menues/start_menu.gd
@@ -5,3 +5,6 @@ func _on_start_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menues/settings_menu.tscn")
