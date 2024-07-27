5454880e3c4533a3787842533e9f6c7dc7cca24d
Bastien Andrey <bastien.andrey@edu.hefr.ch>
2024-07-27 10:39:10 +0200

    feat: fix shop by hardcoding

--- scripts/maps/shop.gd
+++ scripts/maps/shop.gd
@@ -12,11 +12,11 @@ func _ready():
 	choosen_items.append(item_list.pick_random().instantiate())
 	
 	var item_markers = get_tree().get_nodes_in_group("ItemMarker")
-	
 	var i = 0
 	for item_marker in item_markers:
 		get_tree().root.add_child(choosen_items[i])
 		choosen_items[i].global_position = item_marker.global_position
+		print(item_marker.global_position)
 		i += 1
 
 func _on_area_2d_body_entered(body):
