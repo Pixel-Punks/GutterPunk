a3b6812054c52a448d5e0829bbdeacb82185d4a0
Julmy Dimitri <dim.julmy@gmail.com>
2024-07-27 10:53:12 +0200

    [feat] add

--- scenes/items/meth.gd
+++ scenes/items/meth.gd
@@ -7,6 +7,9 @@ func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
 func _on_interact():
+	var player = get_tree().get_first_node_in_group("real_player_no_cap")
+	player.strength += 1
+	player.overdose.took_drugs()
 	if audio_player :
 		audio_player.play_random()
 	queue_free()
